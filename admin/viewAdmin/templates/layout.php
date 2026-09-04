4fa53898050be58e35d34d7c9d3b80da3005eba5
KRAKENN8 <krakeen99@gmail.com>
2026-09-04 10:47:39 +0300

    New version

--- admin/viewAdmin/templates/layout.php
+++ admin/viewAdmin/templates/layout.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
-    <title>Dashboard</title>
-    <link href="/public/css/bootstrap.css" rel="stylesheet">
-    <link href="/public/css/mystyle.css" rel="stylesheet">
-    <link href="/public/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="public/css/font-awesome.min.css">
-    <!-- SCRIPT -->
-    <script src="/public/js/jquery.min.js"></script>
-    <script src="/public/js/bootstrap.min.js"></script>
-    <script src="/public/js/ajaxupload.3.5.js"></script>
+    <meta charset="UTF-8">
+    <title>Dashboard Admin</title>
+    <!-- CSS -->
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/css/mystyle.css" rel="stylesheet">
+    <link href="public/css/font-awesome.min.css" rel="stylesheet">
+    <!-- JS -->
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/ajaxupload.3.5.js"></script>
 </head>
 <body>
 <div class="container">
@@ -22,11 +22,13 @@ if (isset($_SESSION["userId"]) && isset($_SESSION["sessionId"])) {
             <div class="container-fluid">
             <?php
             echo '<ul class="nav nav-pills pull-right">
-            <li role="button">'.$_SESSION["name"].'
-            <a href="logout" style="display: inline;">Выйти <i class="fa fa-sign-out"></i></a></li></ul>';
+                <li role="button">'.$_SESSION["name"].'
+                <a href="logout" style="display: inline;">Выйти <i class="fa fa-sign-out"></i></a></li>
+            </ul>';
 
             if (isset($_SESSION["status"]) && $_SESSION["status"] == "admin") {
-                echo '<h4><a href="../" target="_blank">Web site News portal</a>';
+                echo '<h4>';
+                echo '<a href="../" target="_blank">Web site News portal</a>';
                 echo ' &#187; <a href="./">Start admin</a>';
                 echo ' &#187; <a href="categoryAdmin">News categories</a>';
                 echo ' &#187; <a href="newsAdmin">News List</a>';
@@ -41,9 +43,15 @@ if (isset($_SESSION["userId"]) && isset($_SESSION["sessionId"])) {
 <?php
 }
 ?>
+
     <div id="content" style="padding-top: 20px;">
-        <?php echo $content; ?>
+        <?php 
+        if (isset($content)) {
+            echo $content; 
+        }
+        ?>
     </div>
+
     <footer class="footer">
         <p>&copy; 2019 Design Admin dashboard <i class="fa fa-child"></i></p>
     </footer>
